6305dc3adcb0157c3a6d520d8eb2eab5a71c6fc5
Renan Dourado <100216339+renandourd0@users.noreply.github.com>
2024-05-13 21:15:12 -0300

    Add files via upload

---
+++ pegar_posicao.py
@@ -0,0 +1,10 @@
+# Esse codigo é para pegar a posição do Mouse para usar no pyautogui
+# Rode o codigo e deixe o mouse em cima do local que você queira para ele clicar :)
+
+import time # Importando as Bibliotecas 
+import pyautogui
+
+time.sleep(5) 
+print(pyautogui.position())
+
+pyautogui.scroll(200)
